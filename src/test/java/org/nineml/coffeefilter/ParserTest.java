2cc48ab283cb016afc9c9250626279c919d46117
Norman Walsh <ndw@nwalsh.com>
2022-02-25 07:16:44 +0000

    Irrelevant whitespace changes

--- src/test/java/org/nineml/coffeefilter/ParserTest.java
+++ src/test/java/org/nineml/coffeefilter/ParserTest.java
@@ -14,13 +14,13 @@ public class ParserTest {
     @Test
     public void parseDate() {
         String input = "date: s?, day, -s, month, (-s, year)? .\n" +
-                        "-s: -\" \"+ .\n" +
-                        "day: digit, digit? .\n" +
-                        "-digit: \"0\"; \"1\"; \"2\"; \"3\"; \"4\"; \"5\"; \"6\"; \"7\"; \"8\"; \"9\".\n" +
-                        "month: \"January\"; \"February\"; \"March\"; \"April\";\n" +
-                        "       \"May\"; \"June\"; \"July\"; \"August\";\n" +
-                        "       \"September\"; \"October\"; \"November\"; \"December\".\n" +
-                        "year: ((digit, digit); -\"'\")?, digit, digit .";
+                "-s: -\" \"+ .\n" +
+                "day: digit, digit? .\n" +
+                "-digit: \"0\"; \"1\"; \"2\"; \"3\"; \"4\"; \"5\"; \"6\"; \"7\"; \"8\"; \"9\".\n" +
+                "month: \"January\"; \"February\"; \"March\"; \"April\";\n" +
+                "       \"May\"; \"June\"; \"July\"; \"August\";\n" +
+                "       \"September\"; \"October\"; \"November\"; \"December\".\n" +
+                "year: ((digit, digit); -\"'\")?, digit, digit .";
 
         InvisibleXmlParser parser = InvisibleXml.getParserFromIxml(input);
 
