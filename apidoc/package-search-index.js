9f4da93520fb2bdee5812431822eb7b5b785aca9
ndw <ndw@nwalsh.com>
2023-07-23 16:17:45 +0000

    Deploying to gh-pages from @ nineml/nineml@abbf19074de02cdf4ba862cdbcdc4f71d20ae0a1 🚀

--- apidoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.nineml.coffeefilter"},{"l":"org.nineml.coffeefilter.exceptions"},{"l":"org.nineml.coffeefilter.trees"},{"l":"org.nineml.coffeefilter.util"}]
